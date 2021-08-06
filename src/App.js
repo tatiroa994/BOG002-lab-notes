c32816775c9694af32929382a8c97285eaa196ef
Tatiroa994 <tatico994@gmail.com>
2021-08-05 21:26:04 -0500

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.scss';
+
+import Login from "./pages/login/Login"
+import Register from './pages/register/Register'
+import Timeline from './pages/timeline/Timeline'
+import NotFound from './pages/not-found/Not-Found'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/login" component={Login} ></Route>
+        <Route exact path="/register" component={Register} ></Route>
+        <Route exact path="/timeline" component={Timeline} ></Route>
+        <Route component={NotFound} ></Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/timeline/Timeline.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        timeline
+      </div>
+    )
+  }
+}
+
+export default Timeline
