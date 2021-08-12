97bbe19a968a01aee73fd13b04c040a55a1da39a
Tatiroa994 <tatico994@gmail.com>
2021-08-12 15:42:13 -0500

    create  component button facebook and component google

---
+++ src/commons/components/Button-facebook/ButtonFacebook.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./ButtonFacebook.scss";
+
+export default function ButtonFacebook() {
+  return (
+    <button className="btn-facebook">
+      <img src="assets/facebook-icon.png" alt="" />
+      <span>continuar con Facebook</span>
+    </button>
+  );
+}
---
+++ src/commons/components/Button-google/ButtonGoogle.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./ButtonGoogle.scss";
+
+export default function ButtonGoogle() {
+  return (
+    <button className="btn-google">
+      <img src="assets/google-icon.png" alt="" />
+      <span>continuar con Google</span>
+    </button>
+  );
+}
