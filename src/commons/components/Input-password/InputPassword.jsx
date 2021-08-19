1cbb425a1df4e43f74579ff53378c9427f5beeeb
Tatiroa994 <tatico994@gmail.com>
2021-08-19 09:37:49 -0500

    create component input

--- src/commons/components/Input-password/InputPassword.jsx
+++ src/commons/components/Input-password/InputPassword.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./InputPassword.scss";
 
-export default function InputPassword() {
+export default function InputPassword(props) {
+  const { onChangeInput, value } = props;
+  const [typeInput, setTypeInput] = useState("password");
+  const [typeIcon, setTypeIcon] = useState("isPassword");
+
+  const visibility = () => {
+    if (typeInput === "password") {
+      setTypeInput("text");
+      setTypeIcon("isText");
+    } else {
+      setTypeInput("password");
+      setTypeIcon("isPassword");
+    }
+  };
+
   return (
-    <React.Fragment>
-      <input type="password" className="input-password" />
-      <div className="img-input"></div>
-    </React.Fragment>
+    <div className="password-container">
+      <input
+        type={typeInput}
+        className="input-password"
+        placeholder="Ingrese contraseña"
+        onChange={onChangeInput}
+        value={value}
+      />
+      <div className={`img-input ${typeIcon}`} onClick={visibility}></div>
+    </div>
   );
 }
--- src/commons/components/Input/Input.jsx
+++ src/commons/components/Input/Input.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { string, func } from 'prop-types';
 import "./Input.scss";
 
 export default function Input(props) {
-  const { textInput } = props;
-  return <input className="input-text" placeholder={textInput} type="text" />;
+  const { textInput, typeInput, onChange, value } = props;
+  return (
+    <input
+      className="input-text"
+      placeholder={textInput}
+      type={typeInput}
+      onChange={onChange}
+      value={value}
+    />
+  );
 }
+
+Input.propTypes = {
+  textInput: string.isRequired,
+  typeInput: string.isRequired,
+  onChange: func.isRequired,
+  value: string.isRequired
+}
+
