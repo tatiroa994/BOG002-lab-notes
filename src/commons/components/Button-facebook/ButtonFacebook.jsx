41327ba9973bb2b87500f3b981ce767263dbf22e
Tatiroa994 <tatico994@gmail.com>
2021-08-25 19:10:46 -0500

    setting btns providers

--- src/commons/components/Button-facebook/ButtonFacebook.jsx
+++ src/commons/components/Button-facebook/ButtonFacebook.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./ButtonFacebook.scss";
 
-export default function ButtonFacebook() {
+export default function ButtonFacebook(props) {
+  const { onClick } = props;
   return (
-    <button className="btn-facebook">
-      <img src="assets/facebook-icon.png" alt="" />
+    <button className="btn-facebook" onClick={onClick}>
+      <img src="assets/facebook-icon.png" alt="Facebook" />
       <span>continuar con Facebook</span>
     </button>
   );
--- src/commons/components/Button-google/ButtonGoogle.jsx
+++ src/commons/components/Button-google/ButtonGoogle.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "./ButtonGoogle.scss";
 
-export default function ButtonGoogle() {
+export default function ButtonGoogle(props) {
+  const { onClick } = props;
   return (
-    <button className="btn-google">
+    <button className="btn-google" onClick={onClick}>
       <img src="assets/google-icon.png" alt="" />
       <span>continuar con Google</span>
     </button>
